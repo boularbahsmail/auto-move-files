68463f602607357414e619dacc71664001edbd8a
OB Codes <obcodex@gmail.com>
2020-10-15 22:14:59 +0100

    initial commit

---
+++ automove.py
@@ -0,0 +1,33 @@
+# auto files move
+# import needs tools
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+import os
+import json
+
+# main class
+class MyHandler(FileSystemEventHandler):
+    i = 1
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            src = folder_to_track + "/" + filename # source of file
+            new_destination = folder_destination + "/" + filename # new dest
+            os.rename(src, new_destination)
+
+# from file to another one
+folder_to_track = '/Users/Mousta/Desktop/one' # from
+folder_destination = '/Users/Mousta/Desktop/two' # to
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
+
+
